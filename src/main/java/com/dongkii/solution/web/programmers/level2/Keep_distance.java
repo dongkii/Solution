9e4c2cbedc53d961422f8983784d94aad94bdd6a
dongkii <31609765+dongkii@users.noreply.github.com>
2022-02-18 11:56:18 +0900

    프로그래머스 Lv2 거리두기 확인하기

--- src/main/java/com/dongkii/solution/web/programmers/level2/Keep_distance.java
+++ src/main/java/com/dongkii/solution/web/programmers/level2/Keep_distance.java
@@ -5,13 +5,12 @@ import java.util.List;
 
 public class Keep_distance {
     public static int[] solution(String[][] places) {
-        int[] answer = {};
-
-        List<int[]> pList = new ArrayList<int[]>();
-        List<int[]> xList = new ArrayList<int[]>();
+        int[] answer = new int[5];
 
         // 2차원 배열 전체
         for(int i = 0; i < places.length; i++) {
+            answer[i] = 1;
+            List<int[]> pList = new ArrayList<int[]>();
 
             // 각 대기실 for
             for(int j = 0; j < 5; j++) {
@@ -19,18 +18,45 @@ public class Keep_distance {
                 // 각 자리
                 for(int k = 0; k < 5; k++) {
                     // P(응시자)들 저장
-                    if("P".equals(places[j][k])) {
+                    if("P".equals(String.valueOf(places[i][j].charAt(k)))) {
                         pList.add(new int[] {j, k});
-                    }
-
-                    if("X".equals(places[j][k])) {
-                        xList.add(new int[] {j, k});
                     }
                 }
             }
-            System.out.println(pList.size());
-            System.out.println(xList.size());
-            System.out.println("----------");
+
+            for(int a = 0; a < pList.size()-1; a++) {
+                for(int b = + 1; b < pList.size(); b++) {
+                    // 맨해튼 거리 공식 |b1 - a1| + |b2 - a2|
+                    int m = Math.abs(pList.get(b)[0] - pList.get(a)[0]) + Math.abs(pList.get(b)[1] - pList.get(a)[1]);
+                    if(m == 1) { // 맨해튼 거리가 1이면 파티션이 있을 수 없음
+                        answer[i] = 0;
+                        break;
+                    }
+
+                    if(m == 2) { // 맨해튼 거리가 2이면 중간에 파티션이 있는지 확인
+                        // 각 P값의 x좌표중 최소값, y좌표중 최소값을 구해 4칸만 비교해서 하나라도 O 가 있으면 거리두기 실패
+                        int min_x = Math.min(pList.get(b)[0], pList.get(a)[0]);
+                        int min_y = Math.min(pList.get(b)[1], pList.get(a)[1]);
+
+                        if(pList.get(b)[0] == pList.get(a)[0] && places[i][min_x].charAt(min_y+1) == 'O') {
+                            answer[i] = 0;
+                            break;
+                        } else if(pList.get(b)[1] == pList.get(a)[1] && places[i][min_x+1].charAt(min_y) == 'O') {
+                            answer[i] = 0;
+                            break;
+                        } else if ((pList.get(b)[0] != pList.get(a)[0] && pList.get(b)[1] != pList.get(a)[1])
+                                    && (places[i][min_x].charAt(min_y) == 'O'
+                                    || places[i][min_x + 1].charAt(min_y) == 'O'
+                                    || places[i][min_x].charAt(min_y + 1) == 'O'
+                                    || places[i][min_x + 1].charAt(min_y + 1) == 'O')) {
+                                answer[i] = 0;
+                                break;
+                        }
+                    }
+                }
+
+                if(answer[i] == 0) break;
+            }
         }
 
         return answer;
