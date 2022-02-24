0d01cdfc63e90efc20b558460906dfcfabea1163
dongkii <ydk0831@gmail.com>
2022-02-24 22:08:11 +0900

    프로그래머스 lv2 방금 그곡

--- src/main/java/com/dongkii/solution/web/programmers/level2/Just_that_song.java
+++ src/main/java/com/dongkii/solution/web/programmers/level2/Just_that_song.java
@@ -2,10 +2,11 @@ package com.dongkii.solution.web.programmers.level2;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Just_that_song {
     public static String solution(String m, String[] musicinfos) {
-        String answer = "";
+        String answer = "(None)";
 
         int max = 0;
 
@@ -13,56 +14,43 @@ public class Just_that_song {
             String start = musicinfos[i].split(",")[0]; // 시작시간
             String end = musicinfos[i].split(",")[1]; // 끝난시간
             String name = musicinfos[i].split(",")[2]; // 곡제목
-            String song = musicinfos[i].split(",")[3]; // 음
-
+            String song = musicinfos[i].split(",")[3]; // 멜로디
 
             // 음악이 재생된 시간을 계산
             int minute = (Integer.parseInt(end.split(":")[0]) - Integer.parseInt(start.split(":")[0])) * 60 + (Integer.parseInt(end.split(":")[1]) - Integer.parseInt(start.split(":")[1]));
 
-            List<String> songList = toStrArr(song);
-            List<String> mList = toStrArr(m);
+            // 각 음을 리스트로 변환
+            String melody = toStrArr(song, minute);
+            String hearM = toStrArr(m, 0);
 
-            int t = 0;
-
-            // 음악이 재생시간동안 나온 음 체크 -> 재생시간동안 나온 음을 문자열리스트에 저장
-            for(int j = 0; j < song.length(); j++) {
-                if(song.charAt(j) == '#') {
-                    String pre = songList.get(t-1);
-                    songList.remove(t-1);
-                    songList.add(pre + "#");
-                } else {
-                    songList.add(String.valueOf(song.charAt(j)));
-                    t++;
-                }
-            }
-
-            t = 0;
-            // 들은 음을 문자열리스트에 저장
-            for(int j = 0; j < m.length(); j++) {
-                if(m.charAt(j) == '#') {
-                    String pre = son
-                }
+            // contains함수를 이용해 들은 멜로디와 일치하는 음악을 찾는다
+            if(melody.contains(hearM) && max < minute) {
+                answer = name;
+                max = minute;
             }
         }
 
         return answer;
     }
 
-    public static List<String> toStrArr(String um) {
-        List<String> umList = new ArrayList<String>();
+    public static String toStrArr(String melody, int minute) {
+        String result = "";
 
-        int j = 0;
-        for(int i = 0; i < um.length(); i++) {
-            if(um.charAt(i) == '#') {
-                String pre = umList.get(j-1);
-                umList.remove(j-1);
-                umList.add(pre + "#");
-            } else {
-                umList.add(String.valueOf(um.charAt(i)));
-                j++;
-            }
+        // #이 붙은 음을 소문자로 치환
+        melody = melody.replace("A#", "a")
+                .replace("B#", "b")
+                .replace("C#", "c")
+                .replace("D#", "d")
+                .replace("E#", "e")
+                .replace("F#", "f")
+                .replace("G#", "g");
+
+        // 멜로디를 반복, 0일경우는 들은 멜로디라 반복하지 않는다.
+        if(minute == 0) minute = melody.length();
+        for(int i = 0; i < minute; i++) {
+            result += melody.charAt(i%melody.length());
         }
 
-        return umList;
+        return result;
     }
 }
