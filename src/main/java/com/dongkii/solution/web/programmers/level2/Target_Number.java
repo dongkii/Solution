be7057d5988d1cc970ef8fa7e5fcd3012ad08434
dongkii <31609765+dongkii@users.noreply.github.com>
2022-03-04 10:50:01 +0900

    프로그래머스 Lv2 타겟넘버

--- src/main/java/com/dongkii/solution/web/programmers/level2/Target_Number.java
+++ src/main/java/com/dongkii/solution/web/programmers/level2/Target_Number.java
@@ -1,9 +1,26 @@
 package com.dongkii.solution.web.programmers.level2;
 
 public class Target_Number {
+
+    public static int answer = 0;
+
     public static int solution(int[] numbers, int target) {
-        int answer = 0;
+        dfs(numbers, numbers[0], 0, target);               // 첫번째 수가 양수라 가정
+        dfs(numbers, numbers[0] * -1, 0, target);   // 첫번째 수가 음수라 가정
 
         return answer;
+    }
+
+    public static void dfs(int[] numbers, int number, int depth, int target) {
+        if(depth == numbers.length-1) { // depth == index  depth가 length-1이면 마지막 인덱스
+            if(number == target) {      // num가 target이랑 같으면 answer 추가
+                answer++;
+            }
+            return;
+        }
+
+        // 재귀
+        dfs(numbers, number + numbers[depth + 1], depth + 1, target); // 다음 수가 양수라 가정할 때
+        dfs(numbers, number - numbers[depth + 1], depth + 1, target); // 다음 수가 음수라 가정할 때
     }
 }
