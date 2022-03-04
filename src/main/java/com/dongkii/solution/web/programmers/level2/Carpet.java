0a41e62e6858344e97c7628494cd41acc40cfcbb
dongkii <31609765+dongkii@users.noreply.github.com>
2022-03-04 11:12:39 +0900

    프로그래머스 Lv2 카펫

--- src/main/java/com/dongkii/solution/web/programmers/level2/Carpet.java
+++ src/main/java/com/dongkii/solution/web/programmers/level2/Carpet.java
@@ -4,9 +4,6 @@ public class Carpet {
     public static int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
 
-        int w = 0;
-        int h = 0;
-
         for(int i = 1; i <= yellow; i++) {
             if(yellow%i == 0) {             // yellow를 i로 나눈 나머지가 0이면
                 if((i+2) * (yellow/i + 2) - yellow == brown) {  // 몫 + 2, i + 2의 넓이가 brown + yellow 일때 값을 구하면된다.
