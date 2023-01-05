f35587896b99effc109debc32f6e09a872cda940
dongkii <31609765+dongkii@users.noreply.github.com>
2023-01-05 09:26:27 +0900

    등수매기기 수정

--- src/main/java/com/dongkii/solution/web/programmers/level0/orderFix.java
+++ src/main/java/com/dongkii/solution/web/programmers/level0/orderFix.java
@@ -6,10 +6,21 @@ public class orderFix {
     public static int[] solution(int[][] score) {
         int[] answer = new int[score.length];
 
+        List<Integer> sList = new ArrayList<>();
+        for(int[] s : score) {
+            sList.add(s[0] + s[1]);
+        }
+        sList.sort(Comparator.reverseOrder());
+
+        for(int i = 0; i < score.length; i++) {
+            answer[i] = sList.indexOf(score[i][0] + score[i][1]) + 1;
+        }
+
+        /*
         HashMap<Integer, Integer> map = new HashMap<>();
 
         for(int i = 0; i < score.length; i++) {
-            int avg = score[i][0] * score[i][1] / 2;
+            int avg = (score[i][0] + score[i][1]) / 2;
             map.put(i+1, avg);
         }
 
@@ -22,10 +33,21 @@ public class orderFix {
         });
 
         int i = 1;
+        int pre = 0;
+        int acc = 0;
         for(Map.Entry<Integer, Integer> entry : entryList) {
-            answer[entry.getKey()-1] = i;
+            if(pre != entry.getValue()) {
+                acc = 0;
+            } else {
+                acc += 1;
+            }
+
+            answer[entry.getKey()-1] = (i - acc);
+
             i++;
+            pre = entry.getValue();
         }
+         */
 
         return answer;
     }
